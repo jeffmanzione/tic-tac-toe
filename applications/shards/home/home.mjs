b7c3919512d485b7cc16aedc4e22e44a028bcc6d
jmanzion <jessmanzione101@gmail.com>
2020-06-18 23:28:50 -0400

    added redirecting based on if logged in for home and login page

--- applications/shards/home/home.mjs
+++ applications/shards/home/home.mjs
@@ -3,19 +3,38 @@ import soynode from 'soynode';
 import Shard from '../../../shard.mjs';
 import { IncomingMessage, OutgoingMessage } from 'http';
 import { State } from '../../../state.mjs';
-import { renderPage } from '../../../util.mjs';
+import { createToken, renderPage } from '../../../util.mjs';
+import { UserState } from '../../../state.mjs';
+
+const USERNAME_QUERY_PARAM_KEY = 'username';
+export const USER_COOKIE_KEY = 'tic-tac-toe-user';
 
 export default class HomeShard extends Shard {
-  /**
-   * @param {!IncomingMessage} req 
-   * @param {!OutgoingMessage} res 
-   * @param {!State} state 
-   * @override
-   */
-  receive(req, res, state) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    renderPage({ res: res, soyTemplateName: 'tictactoe.home' });
-    res.end();
-  }
+    /**
+     * @param {!IncomingMessage} req 
+     * @param {!OutgoingMessage} res 
+     * @param {!State} state 
+     * @param {!Mutator} mutator 
+     * @override
+     */
+    receive(req, res, state) {
+        if (req.method == 'POST' && state.req.url.pathname == '/logout') {
+            this._logOut(res, state, mutator);
+            return;
+        }
+        if (req.method === 'POST' && state.req.url.searchParams.get(USERNAME_QUERY_PARAM_KEY) != null) {
+            this._logIn(res, state, mutator);
+            return;
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+
+        renderPage({
+            res: res,
+            soyTemplateName: 'tictactoe.home',
+            soyTemplateInput: { username: state.user != null ? state.user.username : null },
+            //pathToScssFile: '/applications/shards/login/login.scss',
+        });
+        res.end();
+    }
 }
