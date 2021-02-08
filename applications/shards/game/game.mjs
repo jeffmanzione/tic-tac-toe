3e68ad5b0c13187b065436ce9fc48eb4fb61f071
Jeff <jeffreymanzione@gmail.com>
2021-02-07 17:01:55 -0800

    Added shard sass for pages.

--- applications/shards/game/game.mjs
+++ applications/shards/game/game.mjs
@@ -11,23 +11,22 @@ const USERNAME_QUERY_PARAM_KEY = 'username';
 export const USER_COOKIE_KEY = 'tic-tac-toe-user';
 
 export default class GameShard extends Shard {
-    /**
-     * @param {!IncomingMessage} req 
-     * @param {!OutgoingMessage} res 
-     * @param {!State} state 
-     * @param {!Mutator} mutator 
-     * @override
-     */
-    receive(req, res, state) {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-
-        renderPage({
-            res: res,
-            soyTemplateName: 'tictactoe.game',
-            soyTemplateInput: { username: state.user != null ? state.user.username : null },
-            pathToScssFile: '/applications/shards/game/game.scss',
-        });
-        res.end();
-    }
+  /**
+   * @param {!IncomingMessage} req 
+   * @param {!OutgoingMessage} res 
+   * @param {!State} state 
+   * @param {!Mutator} mutator 
+   * @override
+   */
+  receive(req, res, state) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    renderPage({
+      res: res,
+      soyTemplateName: 'tictactoe.game',
+      soyTemplateInput: { 'username': state.user != null ? state.user.username : null },
+      pathToScssFile: '/applications/shards/game/game.scss',
+    });
+    res.end();
+  }
 }
