de09f21bf5d7ba240cfa003b0859fb8a716c4d2b
Jeff <jeffreymanzione@gmail.com>
2021-02-11 16:35:39 -0800

    Now includes package.json

--- main.mjs
+++ main.mjs
@@ -2,9 +2,14 @@ import TicTacToeApp from './applications/tick_tac_toe_app.mjs';
 import Server from './server.mjs';
 import { initSoy } from './util.mjs';
 
+
+const args = process.argv.slice(2);
+
+const hostname = args.length > 0 ? args[0] : 'localhost';
+
 initSoy().then((success, rejection) => {
   new Server({
-    hostname: '10.0.0.14',
+    hostname: hostname,
     applications: {
       8080: new TicTacToeApp(),
     }
